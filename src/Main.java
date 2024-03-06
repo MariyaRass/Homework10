fe8360179021dcd64db955739da8f275fde34b6b
Мария <git@github.com:MariyaRass/helloworld.git>
2024-03-06 12:26:58 +0300

    10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String separator = "";
+        String fullName = lastName + separator + firstName + separator + middleName;
+        System.out.println("Ф. И. О. сотрудника — "+fullName);
+
+        System.out.println("Задание 2");
+        System.out.println("Данные Ф.И.О. сотрудника при заполнении отчета - "+fullName.toUpperCase());
+
+        System.out.println("Задание 3");
+        fullName= "Иванов Семён Семёнович".replace(oldChar:'ё', newChar 'e');
+        System.out.println("Данные Ф.И.О. сотрудника - "+fullName);
+
+
     }
 }
